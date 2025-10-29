16549638d20bb7e1e0bc6381744b96c1b76ee5f7
Leandro <leandroserrano777@gmail.com>
2025-10-29 15:06:16 -0300

    Implementacion de estructura BASE

---
+++ Estructura.cpp
@@ -0,0 +1,52 @@
+#include "Estructura.h"
+#include <iostream>
+using namespace std;
+
+
+    Estructura::Estructura(float posX, float posY)
+    : _vida(100.f)
+    {
+
+
+    if (!_textura.loadFromFile("Bloques.png")) {
+        std::cout << "Error cargando textura" << std::endl;
+    }
+    _sprite.setTexture(_textura);
+    _sprite.setTextureRect(sf::IntRect(0, 0, 32, 32));
+    _sprite.setPosition(posX, posY);
+
+    _colision.setColision(_sprite.getGlobalBounds());
+    _colision.setID("Estructura");
+
+    }
+
+     void Estructura::draw(sf::RenderTarget& target, sf::RenderStates states) const
+     {
+         states.transform *= getTransform();
+        target.draw(_sprite, states);
+     }
+
+
+
+
+
+    void Estructura::recibirGolpe(float danio)
+    {
+        _vida -= danio;
+        if (_vida < 0.0f){
+            _vida = 0.0f;
+        }
+    }
+
+    bool Estructura::estaDestruido() const {
+        return _vida <= 0.0f;
+}
+
+
+
+
+
+//
+//
+//    void update();
+
---
+++ Estructura.h
@@ -0,0 +1,27 @@
+#pragma once
+#include "SFML/Graphics.hpp"
+#include "Colisionador.h"
+
+class Estructura : public sf::Transformable, public sf::Drawable
+{
+
+
+private:
+
+    float _vida;
+    Colisionador _colision;
+    sf::Sprite _sprite;
+    sf::Texture _textura;
+
+public:
+
+    Estructura(float porX, float posY);
+    virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const;
+    void recibirGolpe(float danio);
+    Colisionador &getColisionador() {return _colision;};
+    void update();
+    bool estaDestruido() const;
+
+
+
+};
