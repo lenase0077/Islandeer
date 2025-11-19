111d95585fd7b7f8488163820dcfef58bad0543d
Alejo <alemartinez523123@gmail.com>
2025-11-19 20:43:28 -0300

    cheto mal
    19-11

--- Menu_Principal.h
+++ Menu_Principal.h
@@ -13,30 +13,36 @@ enum class OpcionMenu {
     Salir
 };
 
-enum class SubMenu{
+enum class EstadoMenu{
     Principal,
     SeleccionOpciones,
     ModificadorVolumen
 };
 
-class MenuPrincipal : public sf::Drawable, sf::Transformable{
+class MenuPrincipal : public sf::Drawable, public sf::Transformable{
 private:
+    EstadoMenu _estadoMenu = EstadoMenu::Principal;
     sf::Texture _texturaLogo;
     sf::Texture _texturaBotonJugar;
     sf::Texture _texturaBotonOpciones;
     sf::Texture _texturaBotonSalir;
     sf::Texture _texturaBotonVolumenGeneral;
+    sf::Texture _texturaBotonVolver;
     sf::Texture _background;
 
     sf::Image _imagenLogo;
     sf::Image _imagenJugar;
     sf::Image _imagenOpciones;
     sf::Image _imagenSalir;
+    sf::Image _imagenVolumenGeneral;
+    sf::Image _imagenVolver;
 
     sf::Sprite _spriteLogo;
     sf::Sprite _spriteBotonJugar;
     sf::Sprite _spriteBotonOpciones;
     sf::Sprite _spriteBotonSalir;
+    sf::Sprite _spriteBotonVolumenGeneral;
+    sf::Sprite _spriteBotonVolver;
     sf::Sprite _spriteBackground;
 
     sf::SoundBuffer bufferStardew;
@@ -46,18 +52,20 @@ private:
     float _escalaBotonJugar = 1.0f;
     float _escalaBotonOpciones = 1.0f;
     float _escalaBotonSalir = 1.0f;
+    float _escalaBotonVolumenGeneral = 1.0f;
+    float _escalaBotonVolver = 1.0f;
 
     float _rotacionBotonJugar = 0.0f;
     float _rotacionBotonOpciones = 0.0f;
     float _rotacionBotonSalir = 0.0f;
+    float _rotacionBotonVolumenGeneral = 0.0f;
+    float _rotacionBotonVolver = 0.0f;
 
-    bool _mostrarOpciones = false;
     sf::Font _fuente;
     sf::Text _textoTituloVolumen;
     sf::Text _textoValorVolumen;
     sf::Text _botonMenos;
     sf::Text _botonMas;
-    sf::Text _botonVolver;
 
 public:
     virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const override;
