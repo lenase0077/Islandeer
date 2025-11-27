e7ad0c6307ea901e97dece0d9fcebea8927962ba
Alejo <alemartinez523123@gmail.com>
2025-11-27 20:33:32 -0300

    sdjkdshjksdhjkds
    Sebas gil tendrias que estar haciendo el cofre
    
    Co-Authored-By: Leandro <45925932+lenase0077@users.noreply.github.com>

--- FabricaEstructuras.cpp
+++ FabricaEstructuras.cpp
@@ -3,8 +3,6 @@
 #include <memory>
 using namespace std;
 
-
-
 FabricaEstructuras::FabricaEstructuras(){
     if(!_texturaEstructuras.loadFromFile("Bloques.png")) {
         cout << "Error cargando textura Bloques.png" << endl;
