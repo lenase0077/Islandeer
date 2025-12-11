64b8f4552f327d192db0fab488b29b018c120579
SebastianDCoder <sebastiandurazzini@gmail.com>
2025-12-11 12:02:47 -0300

    Animaciones actualizadas + origen
    las animaciones y el origen parecen estar bien, el problema radica en que necesitamos adaptar el resto de elementos a este nuevo origen.

--- Funciones.cpp
+++ Funciones.cpp
@@ -12,7 +12,6 @@ void setFrame(int fila , int columna, sf::Sprite& sprite){
 void setFramePersonaje(int fila , int columna, sf::Sprite& sprite){
 
     sprite.setTextureRect((sf::IntRect(columna * 32 , fila * 32 , 32 , 32)));
-
 }
 
 float calcularDistancia(const sf::Vector2f& pos1, const sf::Vector2f& pos2)
