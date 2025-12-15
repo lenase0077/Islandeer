7c924f6f2dd71150563b6fd9c87b8834ba998ee3
Leandro <leandroserrano777@gmail.com>
2025-12-15 16:03:24 -0300

    Guardar y cargar partida done

--- Game.cpp
+++ Game.cpp
@@ -101,6 +101,7 @@ void Game::run()
     inv.agregarItem(32,16);
     inv.agregarItem(33,16);
     inv.agregarItem(28,1);
+    inv.agregarItem(42,1);
 
 
     inv.agregarItem(25,16);
@@ -268,7 +269,6 @@ void Game::run()
 
             if (opcion == OpcionMenu::Jugar)
             {
-                cargarPartida();
                 cout << "Anotacion de Lean - Estado jugar carga el personaje" << endl;
 
                 _estadoActual = EstadoJuego::Jugando;
@@ -315,6 +315,12 @@ void Game::run()
             {
                 guardarPartida();
                 cout << "Partida guardada desde el Menu de Opciones" << endl;
+            }
+
+            else if (opcion == OpcionMenu::Cargar)
+            {
+                cargarPartida();
+                cout << "Partida cargada desde el Menu de Opciones" << endl;
             }
 
             else if (opcion == OpcionMenu::Salir)
