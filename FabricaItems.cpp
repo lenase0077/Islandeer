dcce17cc9a403e5fa8427396c3def3d46e3ab2fc
Leandro <leandroserrano777@gmail.com>
2025-12-15 17:28:40 -0300

    backujp

--- FabricaItems.cpp
+++ FabricaItems.cpp
@@ -108,6 +108,8 @@ std::unique_ptr<Item> FabricaItems::crearItem(int id)
         }
 
         if (esHacha) herramienta->setTipoSonido(SonidoHerramienta::HACHA);
+        if (esPico) herramienta->setTipoSonido(SonidoHerramienta::PICO);
+        if (esEspada) herramienta->setTipoSonido(SonidoHerramienta::ESPADA);
 
         // Cargamos titulo/descripcion si existen en el OTRO json
         // IMPORTANTE: Usamos idStr, NO id
